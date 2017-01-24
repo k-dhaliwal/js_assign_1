ddf896d344bf323bcb7815c13da1e68572b98fec
k-dhaliwal <bmwcar987@gmail.com>
2017-01-24 22:14:53 +0000

    first commit

--- assignmnt3.js
+++ assignmnt3.js
@@ -5,13 +5,10 @@ myArr.push(Number(myVal[3]))
 let sum= []
 sum= (Number(myVal[2])+Number(myVal[3]))
 
-if(sum>10){
-console.log("sum is greater than 10")}
-else if(sum>10 && sum<100)
-{ console.log("sum is less than 100")
+if(sum>10 && sum<100)
+{ console.log("sum is greater than 10 and  less than 100")
 }
 else if
 (sum>100 && sum<1000)
 { console.log("sum is greater than 100 but less than 1000")
 }
-else{console.log("others")}
